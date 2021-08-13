524dbd367ecc66e2d8ec654e084575359b9b6fc9
bioerrorlog <n.iijima.0x01@gmail.com>
2021-08-14 00:35:45 +0900

    add __eq__

--- chap_01/src/money/dollar.py
+++ chap_01/src/money/dollar.py
@@ -5,6 +5,9 @@ class Dollar:
     def __init__(self, amount: int) -> None:
         self.amount = amount
 
+    def __eq__(self, other: object) -> bool:
+        return self.__dict__ == other.__dict__
+
     def times(self, multiplier: int) -> None:
         return Dollar(self.amount * multiplier)
 
--- chap_01/tests/money/test_money.py
+++ chap_01/tests/money/test_money.py
@@ -4,9 +4,9 @@ from src.money.dollar import Dollar
 def test_multiplication():
     five = Dollar(5)
     product = five.times(2)
-    assert product.amount == 10
+    assert product == Dollar(10)
     product  = five.times(3)
-    assert product.amount == 15
+    assert product == Dollar(15)
 
 def test_equality():
     assert Dollar(5).equals(Dollar(5))
