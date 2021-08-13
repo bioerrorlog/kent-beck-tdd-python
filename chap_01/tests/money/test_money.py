d2f3a4bc2f96633c46b3d8fe16b34730bb052a16
bioerrorlog <n.iijima.0x01@gmail.com>
2021-08-13 18:45:22 +0900

    add gitignore

--- chap_01/tests/money/test_money.py
+++ chap_01/tests/money/test_money.py
@@ -1,7 +1,6 @@
 from src.money.dollar import Dollar
 
-class MoneyTest:
-    def test_multiplication():
-        five = Dollar(5)
-        five.times(2)
-        assert five.amount == 10
+def test_multiplication():
+    five = Dollar(5)
+    five.times(2)
+    assert five.amount == 10
