c067c1e09087b7ae9eec483552cc7f712d542e20
bioerrorlog <n.iijima.0x01@gmail.com>
2021-08-15 12:05:27 +0900

    delete type hints

--- chap_01/src/money/dollar.py
+++ chap_01/src/money/dollar.py
@@ -1,14 +1,14 @@
 
 
 class Dollar:
-    def __init__(self, amount: int) -> None:
+    def __init__(self, amount):
         self.amount = amount
 
-    def __eq__(self, other: object) -> bool:
+    def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
-    def times(self, multiplier: int) -> None:
+    def times(self, multiplier):
         return Dollar(self.amount * multiplier)
 
-    def equals(self, object) -> bool:
+    def equals(self, object):
         return self.amount == object.amount 
